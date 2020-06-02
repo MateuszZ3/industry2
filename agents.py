8b0b409c1ccf2192eb553b8cf3146dd273d87eb1
Mateusz Zielinski <matzielinski15@gmail.com>
2020-06-03 01:59:41 +0200

    Move all agents to agents.py

--- agents.py
+++ agents.py
@@ -1,22 +1,324 @@
 import asyncio
+import datetime
+import random
+from asyncio import sleep
 from collections import defaultdict
 from dataclasses import dataclass
+from heapq import heappop, heappush
+from typing import List
 
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
-from spade.behaviour import OneShotBehaviour, PeriodicBehaviour
+from spade.behaviour import OneShotBehaviour, CyclicBehaviour, PeriodicBehaviour
 from spade.message import Message
 from spade.template import Template
 
 import settings
 from common import GoMOrder
+from common import Order, Point
 from enums import Operation
 
 
 @dataclass
 class Machine:
     operation: Operation
-    working: bool
+    working: bool = True
+
+
+@dataclass
+class GoMInfo:
+    jid: str
+    machines: List[Machine]
+
+
+class OrderFactory:
+    """
+    Creates `Orders`.
+    """
+
+    def __init__(self):
+        self.unused_id = 1
+        self.op_list = list(Operation)
+
+    def create(self) -> Order:
+        ops = [random.choice(self.op_list) for i in range(4)]
+        order = Order(
+            priority=1,
+            order_id=self.unused_id,
+            current_operation=0,
+            operations=ops
+        )
+
+        self.unused_id += 1
+
+        return order
+
+
+class FactoryAgent(Agent):
+    class StartAgents(OneShotBehaviour):
+        """
+        Starts all other agents.
+        """
+
+        async def run(self):
+            gom_infos = []
+            for i, (gom_jid, tr_jid) in enumerate(self.agent.jids, start=1):
+                gom_operations = list(Operation)
+                gom_infos.append((gom_jid, gom_operations))
+                gom = GroupOfMachinesAgent(manager_address=self.agent.manager_jid, tr_address=tr_jid,
+                                           machines=gom_operations, jid=gom_jid, password=settings.PASSWORD)
+                await gom.start()
+                print(f'gom started {gom_jid=}')
+                await asyncio.sleep(settings.AGENT_CREATION_SLEEP)
+                tr = TransportRobotAgent(position=self.agent.tr_positions[i], gom_address=gom_jid,
+                                         factory_map=self.agent.factory_map, jid=tr_jid, password=settings.PASSWORD)
+                await tr.start()
+                print(f'tr started {tr_jid=}')
+                await asyncio.sleep(settings.AGENT_CREATION_SLEEP)
+
+            manager = Manager(factory_jid=str(self.agent.jid), gom_infos=gom_infos, jid=self.agent.manager_jid,
+                              password=settings.PASSWORD)
+            await manager.start()
+            print('manger')
+
+    class OrderBehav(PeriodicBehaviour):
+        """
+        Cyclically generates orders and sends them to Manager Agent.
+        """
+
+        async def run(self):
+            await self.agent.start_behaviour.join()
+            print('after start')
+            # print(f"Running {type(self).__name__}...")
+
+            order = self.agent.order_factory.create()
+            self.agent.orders[order.order_id] = order
+
+            # Send request
+            msg = Message(to=self.agent.manager_jid)
+            msg.set_metadata("performative", "request")
+            msg.body = order.to_json()  # Set the message content
+
+            await self.send(msg)
+            print(f"Message sent!\n{msg}")
+
+            self.agent.update_callback.emit(order.order_id)  # tmp: Emit progress signal with int
+
+    class OrderAgreeHandler(CyclicBehaviour):
+        """
+        On `agree` from `Manager`.
+        """
+
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            if msg is not None:
+                print(msg)
+
+    class OrderFailureHandler(CyclicBehaviour):
+        """
+        On `failure` from `Manager`.
+        """
+
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            if msg is not None:
+                print(msg)
+
+    class OrderDoneHandler(CyclicBehaviour):
+        """
+        On `inform` from `Manager`.
+        """
+
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            if msg is not None:
+                print(msg)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Orders
+        self.unused_id = 1
+        self.orders = {}
+        self.order_factory = OrderFactory()
+        self.order_behav = None
+
+        self.update_callback = None
+
+        self.manager_jid = f"{settings.AGENT_NAMES['manager']}@{settings.HOST}"
+
+        # GoM IDs start with 1, so that 0 can be used as set-aside's ID
+        self.gom_count = 12
+        self.gom_positions = [Point(x=-128.0, y=0.0)]  # [0] is set-aside position
+        self.tr_positions = [None]  # [0] is a placeholder
+        self.factory_map = {
+            '' : self.gom_positions[0]
+        }
+        self.jids = self.create()
+
+        # Behaviours
+        start_at = datetime.datetime.now() + datetime.timedelta(seconds=5)
+        self.start_behaviour = self.StartAgents()
+        self.order_behav = self.OrderBehav(5.0, start_at)
+        self.agr_handler = self.OrderAgreeHandler()
+        self.fail_handler = self.OrderFailureHandler()
+        self.done_handler = self.OrderDoneHandler()
+
+    async def setup(self):
+        print(f"TickerAgent started at {datetime.datetime.now().time()}")
+        if self.update_callback is None:
+            raise Exception("update_callback not set")
+
+        # Behaviours
+        self.add_behaviour(self.start_behaviour)
+
+        self.add_behaviour(self.order_behav)
+
+        agr_temp = Template()
+        agr_temp.sender = self.manager_jid
+        agr_temp.metadata = {"performative": "agree"}
+        self.add_behaviour(self.agr_handler, agr_temp)
+
+        fail_temp = Template()
+        fail_temp.sender = self.manager_jid
+        fail_temp.metadata = {"performative": "failure"}
+        self.add_behaviour(self.fail_handler, fail_temp)
+
+        done_temp = Template()
+        done_temp.sender = self.manager_jid
+        done_temp.metadata = {"performative": "inform"}
+        self.add_behaviour(self.done_handler, done_temp)
+
+        self.add_behaviour(self.StartAgents())
+
+    def set_update_callback(self, callback) -> None:
+        """
+        Sets callback used for updating GUI.
+        :param callback: callback
+        """
+        self.update_callback = callback
+
+    def create(self):
+        per_col = 5
+        spray_diameter = 10
+        jids = []
+        for i in range(self.gom_count):
+            y = (i % per_col) * 48 - 96
+            x = int(i / per_col) * 64 - 32
+            xo = random.gauss(0, spray_diameter)
+            yo = random.gauss(0, spray_diameter)
+            self.gom_positions.append(Point(x=x, y=y))
+            self.tr_positions.append(Point(x=x + xo, y=y + yo))
+            gom_jid = f"{settings.AGENT_NAMES['gom_base']}{i + 1}@{settings.HOST}"
+            tr_jid = f"{settings.AGENT_NAMES['tr_base']}{i + 1}@{settings.HOST}"
+            jids.append((gom_jid, tr_jid))
+            self.factory_map[gom_jid] = Point(x=x, y=y)
+        return jids
+
+
+class Manager(Agent):
+    class MainLoop(CyclicBehaviour):
+        """Main agent loop"""
+        async def on_start(self):
+            print("Starting main loop . . .")
+
+        async def run(self):
+            if not self.agent.gom_infos:
+                await sleep(settings.MANAGER_LOOP_TIMEOUT)
+                return
+            gom: GoMInfo = random.choice(self.agent.gom_infos)
+            while not self.agent.orders:
+                await sleep(0.1)
+            order: Order = heappop(self.agent.orders)
+            print(order)
+            last = self.agent.last_operation_location[
+                order.order_id] if order.order_id in self.agent.last_operation_location else ''
+            gorder = GoMOrder(order.priority, order.order_id, last, order.operations[order.current_operation])
+            self.agent.active_orders[order.order_id] = order
+            msg = Message(to=gom.jid)
+            msg.set_metadata("performative", "request")
+            msg.body = gorder.to_json()
+            msg.thread = str(order.order_id)
+            print(f'manager send: {msg}')
+            await self.send(msg)
+
+        async def on_end(self):
+            print(f"{self.agent} finished main loop with exit code {self.exit_code}.")
+
+    class OrderRequestHandler(CyclicBehaviour):
+        """Request from factory"""
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            order = Order.from_json(msg.body)
+            print(order)
+            heappush(self.agent.orders, order)
+            reply = Message(self.agent.factory_jid)
+            reply.set_metadata("performative", "agree")
+            await self.send(reply)
+
+    class OrderRefuseHandler(CyclicBehaviour):
+        """Refuse from GoM"""
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            oid = int(msg.thread)
+            order: Order = self.agent.active_orders[oid]
+            heappush(self.agent.orders, order)
+            self.agent.active_orders[oid] = None
+
+    class OrderAgreeHandler(CyclicBehaviour):
+        """Agree from GoM"""
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            print('agree received for order ' + msg.thread)
+            return
+
+    class OrderDoneHandler(CyclicBehaviour):
+        """Inform from GoM"""
+        async def run(self):
+            msg = await self.receive(timeout=settings.RECEIVE_TIMEOUT)
+            oid = msg.thread
+            order: Order = self.agent.active_orders[int(oid)]
+            order.current_operation += 1
+            self.agent.last_operation_location[oid] = msg.sender
+            if not order.is_done():
+                heappush(self.agent.orders, order)
+            else:
+                self.agent.last_operation_location[oid] = None
+                self.agent.active_orders[oid] = None
+                report = Message(self.agent.factory_jid)
+                report.set_metadata("performative", "inform")
+                report.thread = oid
+                await self.send(report)
+
+    def __init__(self, factory_jid: str, gom_infos, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gom_infos = []
+        for gom_jid, operations in gom_infos:
+            self.gom_infos.append(GoMInfo(jid=gom_jid, machines=[Machine(operation=op) for op in operations]))
+        self.orders = []
+        self.active_orders = {}
+        self.last_operation_location = {}
+        self.factory_jid = factory_jid
+        self.main_loop = self.MainLoop()
+        self.req_handler = self.OrderRequestHandler()
+        self.ref_handler = self.OrderRefuseHandler()
+        self.agr_handler = self.OrderAgreeHandler()
+        self.done_handler = self.OrderDoneHandler()
+
+    async def setup(self):
+        print("Manager starting . . .")
+        fac_temp = Template()
+        fac_temp.sender = self.factory_jid
+        fac_temp.metadata = {"performative": "request"}
+        self.add_behaviour(self.req_handler, fac_temp)
+        ref_temp = Template()
+        ref_temp.metadata = {"performative": "refuse"}
+        self.add_behaviour(self.ref_handler, ref_temp)
+        agr_temp = Template()
+        agr_temp.metadata = {"performative": "agree"}
+        self.add_behaviour(self.agr_handler, agr_temp)
+        done_temp = Template()
+        done_temp.metadata = {"performative": "inform"}
+        self.add_behaviour(self.done_handler, done_temp)
+        self.add_behaviour(self.main_loop)
 
 
 class RecvBehaviour(CyclicBehaviour):
