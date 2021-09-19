3af0b50804bfa9730a34ee3c1f2ab27d92c98483
Pawel Fornalik <pawelfor98.67lo@gmail.com>
2021-09-19 11:17:59 +0200

    fix running on Windows

--- industry2/__main__.py
+++ industry2/__main__.py
@@ -1,8 +1,14 @@
+import asyncio
+import sys
+
 from PyQt5.QtWidgets import QApplication
 
 from industry2.factory_gui import MainWindow
 
+
 if __name__ == '__main__':
+    if sys.platform == 'win32':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     app = QApplication([])
     window = MainWindow()
 
