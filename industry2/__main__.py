fd7f03ae68069ac163b7d23a62c9735aabcccfad
Pawel Fornalik <pawelfor98.67lo@gmail.com>
2021-09-22 19:25:01 +0200

    add spade logging

--- industry2/__main__.py
+++ industry2/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import sys
 
 from PyQt5.QtWidgets import QApplication
@@ -10,6 +11,7 @@ if __name__ == '__main__':
     # fix needed for asyncio on Windows [https://github.com/tornadoweb/tornado/issues/2608#issuecomment-550180288]
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    logging.basicConfig(filename='myapp.log', filemode='w', level=logging.DEBUG)
     app = QApplication([])
     window = MainWindow()
 
